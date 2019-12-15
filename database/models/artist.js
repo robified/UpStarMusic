e3459ad0b596f0e68cd1fa8df00aa0b29b05edec
Robin <daorob@gmail.com>
2019-12-14 18:05:38 -0600

    Create artist schema.

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,22 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    webiste: String,
+    netWorth: Number,
+    labelName: String,
+    retired: Boolean,
+    albums: [AlbumSchema]
+});
+
+// turn schema into a model
+const Artist = mongoose.model('artist', ArtistSchema);
+
+// export the model the created model
+module.exports = Artist;
