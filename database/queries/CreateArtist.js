55f8c0502ae1480395e8c761543385e73bd99770
Robin <daorob@gmail.com>
2019-12-14 19:17:18 -0600

    Complete creating an artist.

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -1,9 +1,13 @@
 const Artist = require('../models/artist');
 
 /**
- * Finds a single artist in the artist collection.
+ * Creates a single artist in the artist collection.
  * @param {object} artistProps - Object containing a name, age, yearsActive, and genre
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+    // Previous example new User({ name: 'Joe' });
+    const artist = new Artist(artistProps);
+    
+    return artist.save();
 };
