65665943d7b6854ed7f4e1638250d9bd0f3214f4
Robin <daorob@gmail.com>
2019-12-15 03:19:14 -0600

    Complete retrieving artists, filtering results by name|age|albums released, and applying offset and limit options.

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,22 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+    // write a query that will follow sort, offset, limit options only
+    // it should look like this: { all: [artist], count: count, offset: offset, limit: limit }
+
+    const query = Artist.find({})
+        // to string interpolate an object key
+        .sort({ [sortProperty]: 1 })
+        .skip(offset)
+        .limit(limit);
+
+    return Promise.all([query, Artist.count()])
+        .then((results) => {
+            return { 
+                all: results[0],
+                count: results[1], 
+                offset: offset, 
+                limit: limit 
+            }
+        });
 };
