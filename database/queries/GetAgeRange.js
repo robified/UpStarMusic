ed72ba7b61773ee2777860e065978d33f92d265e
Robin <daorob@gmail.com>
2019-12-14 21:25:17 -0600

    Complete getting the age range and years active range for all artists.

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,23 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+        .find({})
+        .sort({ age: 1 })
+        // will still be an array
+        .limit(1)
+        // still need to pick out one element from the array
+        .then(artists => artists[0].age);
+    
+    const maxQuery = Artist
+        .find({})
+        .sort({ age: -1 })
+        .limit(1)
+        .then(artists => artists[0].age);
+
+    // same thing again, this returns an array
+    return Promise.all([minQuery, maxQuery])
+        .then((result) => {
+            return { min: result[0], max: result[1] };
+        });
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,23 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+    .find({})
+    .sort({ yearsActive: 1 })
+    // will still be an array
+    .limit(1)
+    // still need to pick out one element from the array
+    .then(artists => artists[0].yearsActive);
+
+const maxQuery = Artist
+    .find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+
+// same thing again, this returns an array
+return Promise.all([minQuery, maxQuery])
+    .then((result) => {
+        return { min: result[0], max: result[1] };
+    });
 };
