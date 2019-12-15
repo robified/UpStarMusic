54f97a4fdc406e3cffa40b73ad1ea176d79a24d7
Robin <daorob@gmail.com>
2019-12-14 19:35:52 -0600

    Complete deleting an artist.

--- database/queries/DeleteArtist.js
+++ database/queries/DeleteArtist.js
@@ -6,4 +6,13 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves when the record is deleted
  */
 module.exports = (_id) => {
+    // ES6 version can just do '_id' because the key is the same as value
+    // Solution #1
+    return Artist.remove({ _id });
+
+    // Alternative solution: Not a good solution because you're touching the database twice
+    // return Artist.findById(_id)
+    //     .then((artist) => {
+    //         artist.remove();
+    //     });
 };
